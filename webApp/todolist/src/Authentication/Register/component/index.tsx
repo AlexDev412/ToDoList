6c64dde72bb98414d8562f413544ab36625b3c8e
alex <guy-alexandre.eclapier@epitech.eu>
2022-08-09 17:21:34 +0200

    add handlechange & handleclick on form

--- webApp/todolist/src/Authentication/Register/component/index.tsx
+++ webApp/todolist/src/Authentication/Register/component/index.tsx
@@ -1,13 +1,23 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-const formRegister = () => {
+interface IInput {
+    username: string;
+    password: string;
+    // confirmedPassword: string;
+    handleInputUsername : (input: any) => void;
+    handleInputPassword : (input: any) => void;
+    handleClick : () => void;
+}
+
+
+const formRegister = (props : IInput) => {
     return (
         <div>
     <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Enter email" onChange={props.handleInputUsername} value={props.username}/>
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
@@ -15,12 +25,12 @@ const formRegister = () => {
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" placeholder="Password" onChange={props.handleInputPassword} value={props.password}/>
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
       </Form.Group>
-      <Button variant="primary" type="submit">
+      <Button variant="primary" onClick={props.handleClick}>
         Submit
       </Button>
     </Form>
--- webApp/todolist/src/Authentication/Register/register.tsx
+++ webApp/todolist/src/Authentication/Register/register.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import formRegister from './component/index';
+import FormRegister from "./component/index";
 
-class RegisterComponent extends React.Component{
+class RegisterComponent extends React.Component {
+    state =  {
+        username: '',
+        password: '',
+        confirmPassword: '',
+    };
+
+    handleOnChangeUsername = (input : any) => {
+        this.setState({ username: input.target.value});
+    }
+
+    handleOnChangePassword = (input : any) => {
+        this.setState({ password: input.target.value})
+    }
+
+    handleClick = () => {
+        console.log(this.state.username, this.state.password);
+    }
     render() {
         return (
-            formRegister()
+            <FormRegister 
+                username={this.state.username}
+                password={this.state.password}
+                handleInputUsername={(input) => this.handleOnChangeUsername(input)}
+                handleInputPassword={(input) => this.handleOnChangePassword(input)}
+                handleClick={() => this.handleClick()}
+            />
         );
     }
 }
