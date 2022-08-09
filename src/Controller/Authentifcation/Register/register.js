ea1230122f568c720e0ea3a0a5b573d8bcacbdf8
alex <guy-alexandre.eclapier@epitech.eu>
2022-08-09 10:31:05 +0200

    convert function in try catch

--- src/Controller/Authentifcation/Register/register.js
+++ src/Controller/Authentifcation/Register/register.js
@@ -3,29 +3,26 @@ const { checkUserExist } = require('./register.errormanagement');
 
 function register(app) {
     app.post('/register', async (req, res) => {
-        let { username, password } = req.body;
-
-        if (!username || !password) {
-            res.status(400).send({ msg: 'Invalid username or password', status:400 });
-            return;
-        }
-        const result = await checkUserExist(username);
-        if (result) {
-            res.status(400).send({ msg: 'Username already use.', status:400 });
-            return;
-        } else {
-            const user = new Users({
-                    username: username,
-                    password: password
-            });
-    
-            user.save()
-              .then(() => {
+        try {
+            let { username, password } = req.body;
+            if (!username || !password) {
+                res.status(400).send({ msg: 'Invalid username or password', status:400 });
+                return;
+            }
+            const result = await checkUserExist(username);
+            if (result) {
+                res.status(400).send({ msg: 'Username already use.', status:400 });
+                return;
+            } else {
+                let user = new Users({
+                        username: username,
+                        password: password
+                });
+                user.save()
                 res.status(200).send({msg: "User registered successfully.", status : 200});
-              })
-              .catch(err => {
-                res.status(400).send({ msg: err.message, status: 400});
-              })
+            } 
+        } catch (err) {
+            res.status(400).send({msg: err, status : 400});
         }
     });
 }
