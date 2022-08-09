2f73f46aa20bcc2f0a0b8b18e822d60078bdbce3
alex <guy-alexandre.eclapier@epitech.eu>
2022-08-09 15:55:31 +0200

    create form of register

---
+++ webApp/todolist/src/Authentication/Register/register.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import formRegister from './component/index';
+
+class RegisterComponent extends React.Component{
+    render() {
+        return (
+            formRegister()
+        );
+    }
+}
+
+export default RegisterComponent;
