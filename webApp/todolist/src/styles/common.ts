7d803ebd32aeb6de9b6e51a6cccc0a444a9e82e0
Alexandre Eclapier <71248174+AlexDev412@users.noreply.github.com>
2022-08-16 15:12:54 +0200

    Create three box for cards todo, in progress, done (#15)

--- webApp/todolist/src/styles/common.ts
+++ webApp/todolist/src/styles/common.ts
@@ -1,7 +1,13 @@
 const styles = {
     "10-mt": {
         marginTop: "20px"
-    }
+    },
+    "1O-pr": {
+        paddingRight: "10px"
+    },
+    "1O-pl": {
+        paddingLeft: "20px"
+    },
   } as const;
 
   export default styles;
