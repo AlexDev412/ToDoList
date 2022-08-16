704bc9920d051ffae4b4e768e2f4cf58a38e2830
alex <guy-alexandre.eclapier@epitech.eu>
2022-08-16 15:11:42 +0200

    Create three box for cards todo, in progress, done

--- webApp/todolist/src/Board/Component/index.tsx
+++ webApp/todolist/src/Board/Component/index.tsx
@@ -1,10 +1,24 @@
 import styles from '../Component/style';
+import padding from '../../styles/common';
 
 const FormBoard = (any : any) => {
     return (
         <div style={styles.Background}>
             <div style={styles.BoardBackground}>
-                <p>hello</p>
+                <div style={styles.BoardDirection}>
+                <div style={padding['1O-pl']}></div>
+                    <div style={styles.BoardTodo}>
+                        <p>Todo</p>
+                    </div>
+                    <div style={padding['1O-pr']}></div>
+                    <div style={styles.BoardProgress}>
+                        <p>In progress</p>
+                    </div>
+                    <div style={padding['1O-pr']}></div>
+                    <div style={styles.BoardDone}>
+                        <p>Done</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- webApp/todolist/src/Board/Component/style.ts
+++ webApp/todolist/src/Board/Component/style.ts
@@ -14,7 +14,40 @@ const styles = {
         border: '1px',
         borderRadius: '5px',
         boxShadow: 'rgb(0 0 0 / 10%) 0 0 10px',
-      },
+    },
+    BoardDirection: {
+        display: 'flex',
+        alignItems:'center',
+        justifyContent:'center',
+        paddingTop: '10px',
+    },
+    BoardTodo: {
+        backgroundColor:'rgb(235, 236, 240)',
+        flexDirection: 'column',
+        border: '1px',
+        borderRadius: '3px',
+        width: '500px',
+        height: '90vh',
+        margin: '50px 0px 0px 0px',
+    },
+    BoardProgress: {
+        backgroundColor:'rgb(235, 236, 240)',
+        flexDirection: 'column',
+        border: '1px',
+        borderRadius: '3px',
+        width: '500px',
+        height: '90vh',
+        margin: '50px 0px 0px 0px',
+    },
+    BoardDone: {
+        backgroundColor:'rgb(235, 236, 240)',
+        flexDirection: 'column',
+        border: '1px',
+        borderRadius: '3px',
+        width: '500px',
+        height: '90vh',
+        margin: '50px 0px 0px 0px',
+    },
   } as const;
 
   export default styles;
